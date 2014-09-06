7fca8877f647518abb248882075b04812c67269c
Adeel Khan Yusufzai <kadeel@gmail.com>
2014-09-06 12:07:48 +0200

    make sure web_id is set for new revisions

--- app/models/revision.rb
+++ app/models/revision.rb
@@ -3,6 +3,8 @@ class Revision < ActiveRecord::Base
   belongs_to :web
   composed_of :author, :mapping => [ %w(author name), %w(ip ip) ]
 
+  before_save :update_web_id
+
   def content
     read_attribute(:content).as_utf8
   end
@@ -10,5 +12,11 @@ class Revision < ActiveRecord::Base
   def number
     # for long arrays, this is faster than array.index
     Hash[self.page.rev_ids.map.with_index.to_a][self] + 1
+  end
+
+  def update_web_id
+    if !self.web_id and self.page and self.page.web_id
+      self.web_id = self.page.web_id
+    end
   end
 end
