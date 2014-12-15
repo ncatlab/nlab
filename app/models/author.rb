01c21a10922080e7f0de584f53bd9aa8a1107d2b
Adeel Khan <kadeel@gmail.com>
2014-12-15 19:12:55 +0100

    minor

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,7 @@
 class Author < String
   attr_accessor :ip
   attr_reader :name
-  def initialize(name, ip = nil) 
+  def initialize(name, ip = nil)
     @ip = ip
     super(name.as_utf8)
   end
@@ -9,10 +9,28 @@ class Author < String
   def name=(value)
     self.gsub!(/.+/, value)
   end
-  
+
   alias_method :name, :to_s
-  
+
   def <=>(other)
     name <=> other.to_s
+  end
+
+  def sanitize_string(input)
+    if input.is_a? String
+      input = input.dup.force_encoding(Encoding::UTF_8)
+
+      if input.valid_encoding?
+        input
+      else
+        input.
+          force_encoding(Encoding::ASCII_8BIT).
+          encode!(Encoding::UTF_8,
+                  invalid: :replace,
+                  undef:   :replace)
+      end
+    else
+      input
+    end
   end
 end
