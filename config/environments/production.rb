5504028d6586f174b02561592926834aef221f2e
Adeel Khan <kadeel@gmail.com>
2015-01-29 14:03:10 +0100

    show full error messages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ end
 ####
 
 # Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = false
+config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
