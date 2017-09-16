ncatlab/nlab

commit dde3aa9f429d6b24b89b8ce910e80b75f0dcd9b4

Hardcode a lock on the "monoidal categories" page due to spam.
See <https://nforum.ncatlab.org/discussion/4226/monoidal-category/>.

app/controllers/wiki_controller.rb
@@ -259,6 +259,10 @@ EOL
       redirect_home
     elsif @page.locked?(Time.now) and not params['break_lock']
       redirect_to :web => @web_name, :action => 'locked', :id => @page_name
+    elsif @page.id == 231
+      # hardcode a lock on the "monoidal categories" page due to spam
+      flash[:info] = "The page \"#{@page_name}\" is currently locked due to spam."
+      redirect_to :web => @web_name, :action => 'show', :id => @page_name
     else
       @page.lock(Time.now, @author)
     end
@@ -350,6 +354,11 @@ EOL
     return unless is_post
     author_name = params['author'].purify.strip
     author_name = 'Anonymous' if author_name =~ /^\s*$/
+
+    if @page.id == 231
+      # hardcode a lock on the "monoidal categories" page due to spam
+      flash[:info] = "The page \"#{@page_name}\" is currently locked due to spam."
+      redirect_to :web => @web_name, :action => 'show', :id => @page_name
 
     begin
       the_content = params['content'].purify

commit 34b98c93b885cb30c60536bab671b6d766525a7c

Fix a bug preventing creating new pages, which was introduced in dde3aa9f429d6b24b89b8ce910e80b75f0dcd9b4.

app/controllers/wiki_controller.rb
@@ -355,7 +355,7 @@ EOL
     author_name = params['author'].purify.strip
     author_name = 'Anonymous' if author_name =~ /^\s*$/
 
-    if @page.id == 231
+    if not @page.nil? and @page.id == 231
       # hardcode a lock on the "monoidal categories" page due to spam
       flash[:info] = "The page \"#{@page_name}\" is currently locked due to spam."
       redirect_to :web => @web_name, :action => 'show', :id => @page_name