5df70261592c18c80f10d55f29989c4fb65c523e
Christian Sattler <sattler.christian@gmail.com>
2022-05-30 22:37:06 +0200

    Use more meaningful variable names in cache sweepers.

--- app/controllers/revision_sweeper.rb
+++ app/controllers/revision_sweeper.rb
@@ -9,25 +9,28 @@ class RevisionSweeper < ActionController::Caching::Sweeper
   # Also runs after creation.
   def after_save(record)
     if record.is_a?(Revision)
-      expire_cached_page(record.web, record.page.name)
-      expire_cached_summary_pages(record.web)
-      expire_cached_revisions(record.web, record.page.name)
-      expire_related_caches(record.web, record.page.name, record.page)
+      revision = record
+      expire_cached_page(revision.web, revision.page.name)
+      expire_cached_summary_pages(revision.web)
+      expire_cached_revisions(revision.web, revision.page.name)
+      expire_related_caches(revision.web, revision.page.name, revision.page)
     end
   end
 
   def after_create(record)
     if record.is_a?(Page)
-      expire_referencing_caches(record.web, record.name)
+      page = record
+      expire_referencing_caches(page.web, page.name)
     end
   end
 
   def after_delete(record)
     if record.is_a?(Page)
-      expire_cached_page(record.web, record.name)
-      expire_cached_summary_pages(record.web)
-      expire_related_caches(record.web, record.name, record)
-      expire_cached_revisions(record.web, record.name)
+      page = record
+      expire_cached_page(page.web, page.name)
+      expire_cached_summary_pages(page.web)
+      expire_related_caches(page.web, page.name, page)
+      expire_cached_revisions(page.web, page.name)
     end
   end
 
--- app/controllers/web_sweeper.rb
+++ app/controllers/web_sweeper.rb
@@ -12,18 +12,21 @@ class WebSweeper < ActionController::Caching::Sweeper
       web.pages.find_each { |page| expire_cached_page(web, page.name) }
       expire_cached_summary_pages(web)
     elsif record.is_a?(WikiFile)
-      record.web.pages_that_link_to_file(record.file_name).each do |page|
-        expire_cached_page(record.web, page)
+      wiki_file = record
+      wiki_file.web.pages_that_link_to_file(wiki_file.file_name).each do |page|
+        expire_cached_page(wiki_file.web, page)
       end
-      expire_cached_summary_pages(record.web)
+      expire_cached_summary_pages(wiki_file.web)
     end
   end
 
   def after_destroy(record)
     if record.is_a?(Web)
-      expire_cached_summary_pages(record)
-    else
-      expire_cached_summary_pages(record.web)
+      web = record
+      expire_cached_summary_pages(web)
+    elif record.is_a?(WikiFile)
+      wiki_file = record
+      expire_cached_summary_pages(wiki_file.web)
     end
   end
 
