5edcfc4ab4c9c8a2407ba55a804b7e326cc44603
Felix Cherubini <felix.cherubini@posteo.de>
2022-06-21 17:44:37 +0200

    update deprecated function

--- vendor/rails/activesupport/lib/active_support/core_ext/bigdecimal/conversions.rb
+++ vendor/rails/activesupport/lib/active_support/core_ext/bigdecimal/conversions.rb
@@ -13,7 +13,7 @@ module ActiveSupport #:nodoc:
             alias_method :_original_to_s, :to_s
             alias_method :to_s, :to_formatted_s
 
-            yaml_as YAML_TAG
+            yaml_tag YAML_TAG
           end
         end
 
