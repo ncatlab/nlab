ncatlab/nlab

commit c8417e60c4ef5540365aad963c9271c4c2e5154c

Manually expire revision caches.
* Expires also revision pages for invalid revision numbers.
* Speeds up cache expiration on edits of pages with many revisions.
* Rudimentary protection against deletion of files outside the cache directory (missing Rails 2).

app/controllers/cache_sweeping_helper.rb
@@ -40,14 +40,23 @@ module CacheSweepingHelper
   end
 
   def expire_cached_revisions(page)
-    page.rev_ids.count.times  do |i|
-      revno = i+1
-      expire_action :controller => 'wiki', :web => page.web.address,
-          :action => 'revision', :id => page.name, :rev => revno
-      expire_action :controller => 'wiki', :web => page.web.address,
-          :action => 'revision', :id => page.name, :rev => revno, :mode => 'diff'
-      expire_action :controller => 'wiki', :web => page.web.address,
-          :action => 'source', :id => page.name, :rev => revno
+    # Hack to expire all revisions, even the ones created for non-existing revision numbers.
+    # Saves time over manually expiring each revision, in particular for pages with thousands of revision (e.g. Sandbox).
+    [ File.join('revision'),
+      File.join('revision', 'diff'),
+      File.join('source')
+    ].each do |rel_path|
+      base_path = File.join(
+        Rails.configuration.cache_store[1],
+        'views',
+        page.web.address,
+        rel_path,
+      )
+      # Rudimentary protection against file path manipulation attacks:
+      # only take the prefix up until the first slash (Unix only).
+      file_name = page.name.split('/', 2).first
+      path = File.join(base_path, file_name)
+      FileUtils.rm_rf(path)
     end
   end
 

commit b2cacb9e9601369c516bbc3c2360a8ba62f80c10

Fix cache path escaping bug introduced in c8417e60.

app/controllers/cache_sweeping_helper.rb
@@ -61,7 +61,7 @@ module CacheSweepingHelper
       )
       # Rudimentary protection against file path manipulation attacks:
       # only take the prefix up until the first slash (Unix only).
-      file_name = page_name.split('/', 2).first
+      file_name = CGI.escape(page_name).split('/', 2).first
       path = File.join(base_path, file_name)
       FileUtils.rm_rf(path)
     end