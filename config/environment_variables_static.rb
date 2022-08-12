e89b1cfe53bcd3e8701fcd621c8dc409e1fc6408
Christian Sattler <sattler.christian@gmail.com>
2022-08-12 16:26:26 +0200

    Parametrize diagram generator over pdflatex invocation (to allow for sandboxing).

--- config/environment_variables_static.rb
+++ config/environment_variables_static.rb
@@ -40,6 +40,7 @@ ENV["RUN_COMMAND_FOR_LATEX_COMPILER"] = "script/itex2MML"
 # Configuration of the diagram_caching script.
 # See script/src/diagram_caching/diagram_caching.py for documentation.
 ENV["NLAB_DIAGRAM_CACHE_DIRECTORY"] = "diagram_cache"
+ENV["NLAB_PDFLATEX"] = "pdflatex"
 ENV["NLAB_DIAGRAM_TIMEOUT"] = "5"
 ENV["NLAB_DIAGRAM_LATEX_RESTRICT_OPEN"] = "1"
 
