603f94162ed94a21df08a0935ac6855ad713dabf
Christian Sattler <sattler.christian@gmail.com>
2022-10-12 16:15:31 +0200

    Revert "Add search functionality for bibliography"
    This reverts commit 2ac186a8bfb0697dff75835e19f454e0462863f2.

--- app/controllers/reference_controller.rb
+++ app/controllers/reference_controller.rb
@@ -2,26 +2,7 @@ require "date"
 require "json"
 
 class ReferenceController < ApplicationController
-  layout "default", :except => [ "all" ]
-
-  def all
-    search_expression = params[:search]
-    if search_expression.present?
-      @search_expression = search_expression
-    else
-      @search_expression = nil
-    end
-    all_references_path = ENV["NLAB_ALL_REFERENCES_PATH"]
-    response, error_message, status = Open3.capture3(
-        all_references_path)
-    if !status.success?
-      flash[:error] = error_message
-      return
-    end
-    @all_references = response.html_safe
-    render layout: false, content_type: "text/html"
-    return
-  end
+  layout "default", :except => []
 
   def show
      @citation_key = params[:citation_key]
